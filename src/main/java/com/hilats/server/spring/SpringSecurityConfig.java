b05dbf65eaf7791677aab4c7363ca351e2e4d000
pduchesne <pduchesne@gmail.com>
2016-09-10 10:25:02 +0200

    use application.properties to configure security

--- src/main/java/com/hilats/server/spring/SpringSecurityConfig.java
+++ src/main/java/com/hilats/server/spring/SpringSecurityConfig.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.SecurityConfigurer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,8 +22,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@PropertySource("classpath:application.properties")
 @Order(5)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    Environment env;
 
     @Autowired
     private HilatsUserDetailsService userService;
@@ -51,6 +57,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        boolean allowAnonymous = env.getProperty("auth.allowAnonymous", Boolean.class, false);
+
         http
                 .exceptionHandling().and()
                 .anonymous().and()
@@ -59,21 +67,23 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 //.httpBasic().and() //enable basic authentication, but users defined elsewhere
                 .authorizeRequests()
 
-                // Allow anonymous resource requests
-                .antMatchers("/").permitAll()
-                .antMatchers("/favicon.ico").permitAll()
-                .antMatchers("/**/*.html").permitAll()
-                .antMatchers("/**/*.css").permitAll()
-                .antMatchers("/**/*.js").permitAll()
+                    // Allow anonymous resource requests
+                    .antMatchers("/").permitAll()
+                    .antMatchers("/favicon.ico").permitAll()
+                    .antMatchers("/**/*.html").permitAll()
+                    .antMatchers("/**/*.css").permitAll()
+                    .antMatchers("/**/*.js").permitAll()
 
-                // Allow anonymous logins
-                .antMatchers("/api/auth/**").permitAll()
-
-                // All other request need to be authenticated
-                .anyRequest().authenticated()   // comment out to let REST resource do the authorization
+                    // Allow anonymous logins
+                    .antMatchers("/api/auth/**").permitAll()
 
                 // Custom Token based authentication based on the header previously given to the client
                 .and().addFilterBefore(new StatelessAuthenticationFilter(tokenAuthenticationService), UsernamePasswordAuthenticationFilter.class);
+
+        // if anonymous, all authorization is left up to REST resources
+        if (!allowAnonymous)
+            // All other request need to be authenticated
+            http.authorizeRequests().anyRequest().authenticated();
     }
 
     @Override
