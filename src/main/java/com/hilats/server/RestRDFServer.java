46354b2dad6c258a51ecf35587a44c212486018e
pduchesne <pduchesne@gmail.com>
2017-02-04 16:51:36 +0100

    increase proxy timeout

--- src/main/java/com/hilats/server/RestRDFServer.java
+++ src/main/java/com/hilats/server/RestRDFServer.java
@@ -179,7 +179,7 @@ public class RestRDFServer {
         ServletRegistration reg = proxyCtx.addServlet("ProxyServlet", new HilatsProxyServlet());
         reg.setInitParameter("targetUri", "{_uri}");
         // set agressive timeouts to prevent proxy threads jams
-        reg.setInitParameter("httpClient.socketTimeout", "5000");
+        reg.setInitParameter("httpClient.socketTimeout", "15000");
         reg.setInitParameter("httpClient.connectionTimeout", "2000");
         reg.setInitParameter("httpClient.connectionRequestTimeout", "1000");
         reg.addMapping("/proxy");
